ae2746ff2bb86e3f1397fdc38012a3d2f3e16e7a
Raghav Malik <raghav1987@gmail.com>
2015-07-11 19:49:40 +0530

    implement with dryer code

--- app/src/main/java/com/example/raghav/project1/MainActivity.java
+++ app/src/main/java/com/example/raghav/project1/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.raghav.project1;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -9,7 +10,7 @@ import android.widget.Button;
 import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
 
     private Button button2;
     private Button button3;
@@ -17,59 +18,30 @@ public class MainActivity extends ActionBarActivity {
     private Button button5;
     private Button button6;
     private Button button7;
+    private Toast mAppToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("WDQFEW","QWFEF");
         setContentView(R.layout.activity_main);
         button2 = (Button)findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+
         button3 = (Button)findViewById(R.id.button3);
+        button3.setOnClickListener(this);
+
         button4 = (Button)findViewById(R.id.button4);
+        button4.setOnClickListener(this);
+
         button5 = (Button)findViewById(R.id.button5);
+        button5.setOnClickListener(this);
+
         button6 = (Button)findViewById(R.id.button6);
+        button6.setOnClickListener(this);
+
         button7 = (Button)findViewById(R.id.button7);
-
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "This is gonna start my spotify streamer!", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "This is gonna start my scores app!", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "This is gonna start my library app!", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        button5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "This is gonna start my bigger app!", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        button6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "This is gonna start my bacon reader app!", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        button7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "This is gonna start my capstone projecto!", Toast.LENGTH_SHORT).show();
-            }
-        });
+        button7.setOnClickListener(this);
     }
 
 
@@ -93,5 +65,30 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button2:
+                Toast.makeText(MainActivity.this, "This is gonna start my spotify streamer!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button3:
+                Toast.makeText(MainActivity.this, "This is gonna start my scores app!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button4:
+                Toast.makeText(MainActivity.this, "This is gonna start my library app!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button5:
+                Toast.makeText(MainActivity.this, "This is gonna start my bigger app!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button6:
+                Toast.makeText(MainActivity.this, "This is gonna start my bacon reader app!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button7:
+                Toast.makeText(MainActivity.this, "This is gonna start my capstone projecto!", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
     }
 }
