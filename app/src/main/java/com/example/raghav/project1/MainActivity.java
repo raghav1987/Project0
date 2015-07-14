026a3730966e169254d9de1dc0d9638bef6e0c09
Raghav Malik <raghav1987@gmail.com>
2015-07-14 09:59:04 +0530

    refactor to add onClick inside the xml

--- app/src/main/java/com/example/raghav/project1/MainActivity.java
+++ app/src/main/java/com/example/raghav/project1/MainActivity.java
@@ -10,38 +10,12 @@ import android.widget.Button;
 import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity implements View.OnClickListener {
-
-    private Button button2;
-    private Button button3;
-    private Button button4;
-    private Button button5;
-    private Button button6;
-    private Button button7;
-    private Toast mAppToast;
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d("WDQFEW","QWFEF");
         setContentView(R.layout.activity_main);
-        button2 = (Button)findViewById(R.id.button2);
-        button2.setOnClickListener(this);
-
-        button3 = (Button)findViewById(R.id.button3);
-        button3.setOnClickListener(this);
-
-        button4 = (Button)findViewById(R.id.button4);
-        button4.setOnClickListener(this);
-
-        button5 = (Button)findViewById(R.id.button5);
-        button5.setOnClickListener(this);
-
-        button6 = (Button)findViewById(R.id.button6);
-        button6.setOnClickListener(this);
-
-        button7 = (Button)findViewById(R.id.button7);
-        button7.setOnClickListener(this);
     }
 
 
@@ -67,8 +41,7 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public void onClick(View v) {
+    public void startProject(View v) {
         switch (v.getId()) {
             case R.id.button2:
                 Toast.makeText(MainActivity.this, "This is gonna start my spotify streamer!", Toast.LENGTH_SHORT).show();
